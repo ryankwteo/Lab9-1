f0dfd4e7f9e4854cc1de0927c2f03a6d313b1e66
ryankwteo <ryankt2@illinois.edu>
2018-03-27 18:24:13 -0500

    Added own key.

--- src/main/java/APITest.java
+++ src/main/java/APITest.java
@@ -16,7 +16,7 @@ public class APITest {
 
     /** Subscription key. Replace with your API key. */
     private static final String SUBSCRIPTION_KEY =
-            "13hc77781f7e4b19b5fcdd72a8df7156";
+            "63394fe57e5942cea2b3d55c0ed33094";
 
     /**
      * Region to use.
